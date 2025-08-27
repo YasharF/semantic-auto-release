fe3d46a1611125f33c5938fdba6cbc376c99e4b4
Yashar Fakhari <YasharF.Git1@ydftech.com>
2025-08-27 12:12:14 -0700

    fix: changes not change

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,20 @@
 # Changes
 
+# [2.0.0](https://github.com/YasharF/semantic-auto-release/compare/v1.0.0...v2.0.0) (2025-08-27)
+
+
+* feat!: remove pr version bump process ([70781f8](https://github.com/YasharF/semantic-auto-release/commit/70781f83d2e2ae86f73cb9e58c6dc219ce7d5d01))
+
+
+### Features
+
+* rename CHANGELOG.md to CHANGES.md ([90982b0](https://github.com/YasharF/semantic-auto-release/commit/90982b039ff01f1ce43ccb62d2dd581b02c9fb79))
+
+
+### BREAKING CHANGES
+
+* This is a full overhaul. Read Readme for current usage.
+
 # 1.0.0 (2025-08-27)
 
 ### Bug Fixes
--- release.config.js
+++ release.config.js
@@ -32,15 +32,15 @@ module.exports = {
         [
             "@semantic-release/changelog",
             {
-                changelogFile: "CHANGE.md",
-                changelogTitle: "# Changelog",
+                changelogFile: "CHANGES.md",
+                changelogTitle: "# Changes",
             },
         ],
         "@semantic-release/npm",
         [
             "@semantic-release/git",
             {
-                assets: ["package.json", "package-lock.json", "CHANGE.md"],
+                assets: ["package.json", "package-lock.json", "CHANGES.md"],
                 message:
                     "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
             },
