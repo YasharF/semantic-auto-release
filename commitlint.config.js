65cbd2c2bc76e9d49b0bc4b6c192f978b69cac75
Yashar Fakhari <YasharF.Git1@ydftech.com>
2025-08-30 15:59:47 -0700

    style: adjust indentation from 4 to 2

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,27 +1,27 @@
 "use strict";
 module.exports = {
-    extends: ["@commitlint/config-conventional"],
-    rules: {
-        "type-enum": [
-            2,
-            "always",
-            [
-                "feat",
-                "fix",
-                "docs",
-                "style",
-                "refactor",
-                "perf",
-                "test",
-                "build",
-                "ci",
-                "chore",
-                "revert",
-                "config",
-                "infra",
-                "ux",
-                "deps",
-            ],
-        ],
-    },
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+        "config",
+        "infra",
+        "ux",
+        "deps",
+      ],
+    ],
+  },
 };
