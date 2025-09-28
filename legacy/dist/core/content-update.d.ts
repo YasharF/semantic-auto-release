96e801ba97a8025b230f78d4b8af643f142cbaea
Yashar Fakhari <YasharF.Git1@ydftech.com>
2025-09-28 05:27:50 -0700

    refactor: removed unused legacy files

--- legacy/dist/core/content-update.d.ts
+++
@@ -1,8 +0,0 @@
-export interface ContentUpdateInput {
-  version: string | undefined;
-  notes: string;
-  changelogFile?: string;
-}
-export declare function applyContentUpdate(
-  input: ContentUpdateInput,
-): Promise<void>;
